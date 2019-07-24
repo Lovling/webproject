9f1d73bd30f4e7f39c805b82905203e564156471
Lovling <18895627037@163.com>
2019-07-24 09:18:23 +0800

    配置未完成

---
+++ src/components/top.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>头部组件</section>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less" scoped>
+section {
+    width: 100%;
+    height: 106px;
+    background-color: rgb(7,17,27,.5);
+}
+</style>
+
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Top></Top>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Top from "@/components/top.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Top
   }
 };
 </script>
