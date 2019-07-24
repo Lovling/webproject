92d874fab5ebce77520fe3213eebba45089ae71a
Lovling <18895627037@163.com>
2019-07-24 13:51:54 +0800

    引入flexible及fastClick

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import flexible from "flexible.js";
+import fastClick from "fastclick";
 
 Vue.config.productionTip = false;
+// 1rem = 50px, 需要编辑器插件配置一下
+flexible(750, 640);
+// 消除300ms延迟
+fastClick.attach(document.body);
 
 new Vue({
   router,
