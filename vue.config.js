6a304aeb782f0c61ee828cd0ac716e7888d0a18b
Lovling <18895627037@163.com>
2019-07-26 15:28:59 +0800

    修改host

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     productionSourceMap: false,
     devServer: {
         port: 8085, // 端口号
-        host: '172.16.3.12',
+        host: 'localhost',
         https: false, // https:{type:Boolean}
         open: true, //配置自动启动浏览器
         // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
