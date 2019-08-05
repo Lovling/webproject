d5fba30bb0abf097660060e80e31de278ba7ce70
Lovling <18895627037@163.com>
2019-08-05 10:50:55 +0800

    配置别名

--- vue.config.js
+++ vue.config.js
@@ -1,31 +1,63 @@
-module.exports = {
-    publicPath: '',
-    outputDir: 'dist',
-    pages: {
-        index: {
-            entry: 'src/main.js',
-            template: 'public/index.html',
-            filename: 'index.html'
-        }
-    },
-    lintOnSave: true,
-    // 如果您不需要生产时的源映射，那么将此设置为false可以加速生产构建
-    productionSourceMap: false,
-    devServer: {
-        port: 8085, // 端口号
-        host: 'localhost',
-        https: false, // https:{type:Boolean}
-        open: true, //配置自动启动浏览器
-        // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
-        // proxy: {
-        //     '/api': {
-        //         target: '<url>',
-        //         ws: true,
-        //         changeOrigin: true
-        //     },
-        //     '/foo': {
-        //         target: '<other_url>'
-        //     }
-        // },  // 配置多个代理
-    }
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, "./", dir);
 }
+
+module.exports = {
+  publicPath: "",
+  outputDir: "dist",
+  chainWebpack: config => {
+    // 为src下文件配别名，不使用相对路径
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("assets", resolve("src/assets"))
+      .set("components", resolve("src/components"))
+      .set("views", resolve("src/views"))
+      .set("icons", resolve("src/icons"))
+      .set("router", resolve("src/router"))
+      .set("utils", resolve("src/utils"))
+      .set("style", resolve("src/style"));
+
+    /** 设置处理svg的router，使svg可直接用名称调用，无需路径 */
+    // svg rule loader
+    // const svgRule = config.module.rule("svg"); // 找到svg-loader
+    // svgRule.uses.clear(); // 清除已有的loader, 如果不这样做会添加在此loader之后
+    // svgRule.exclude.add(/node_modules/); // 正则匹配排除node_modules目录
+    // svgRule // 添加svg新的loader处理
+    //   .test(/\.svg$/)
+    //   .use("svg-sprite-loader")
+    //   .loader("svg-sprite-loader")
+    //   .options({
+    //     symbolId: "icon-[name]"
+    //   });
+  },
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html"
+    }
+  },
+  lintOnSave: true,
+  // 如果您不需要生产时的源映射，那么将此设置为false可以加速生产构建
+  productionSourceMap: false,
+  devServer: {
+    port: 8085, // 端口号
+    host: "localhost",
+    https: false, // https:{type:Boolean}
+    open: true, //配置自动启动浏览器
+    hotOnly: true // 热更新
+    // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
+    // proxy: {
+    //     '/api': {
+    //         target: '<url>',
+    //         ws: true,
+    //         changeOrigin: true
+    //     },
+    //     '/foo': {
+    //         target: '<other_url>'
+    //     }
+    // },  // 配置多个代理
+  }
+};
